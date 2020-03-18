163c3f07d89a65a2f799367e5fd226ab9f4b7a33
ulx <alexand.rusin@gmail.com>
2020-03-18 17:00:54 +0300

    fix

--- buildSrc/src/main/kotlin/GradleDependency.kt
+++ buildSrc/src/main/kotlin/GradleDependency.kt
@@ -1,10 +1,18 @@
 object GradlePluginVersion {
-    const val ANDROID_GRADLE = "3.5.3"
+    const val ANDROID_GRADLE = "3.6.1"
     const val KTLINT_GRADLE = "9.1.1"
-    const val GRADLE_VERSION_PLUGIN = "0.28.0"
     const val DETEKT = "1.4.0"
+    const val GRADLE_VERSION_PLUGIN = "0.22.0"
     const val KOTLIN = CoreVersion.KOTLIN
     const val SAFE_ARGS = CoreVersion.NAVIGATION
+    const val FRBRIC_GRADLE = "1.31.0"
+    const val GOOGLE_SERVICE_GRADLE = "4.3.3"
+    const val FIREBASE_GRADLE = "1.1.5"
+    const val KOIN_GRADLE = "2.1.3"
+}
+
+object GradlePluginName {
+
 }
 
 object GradlePluginId {
@@ -18,8 +26,13 @@ object GradlePluginId {
     const val KOTLIN_ANDROID_EXTENSIONS = "org.jetbrains.kotlin.android.extensions"
     const val GRADLE_VERSION_PLUGIN = "com.github.ben-manes.versions"
     const val SAFE_ARGS = "androidx.navigation.safeargs.kotlin"
+    const val FABRIC_PLUGIN = "io.fabric.tools:gradle"
+    const val GOOGLE_SERVICE_PLUGIN = "com.google.gms:google-services"
+    const val FIREBASE_PLUGIN = "com.google.firebase:firebase-plugins"
+    const val KOIN_PLUGIN = "org.koin:koin-gradle-plugin"
 }
 
 object GradleOldWayPlugins {
     const val ANDROID_GRADLE = "com.android.tools.build:gradle:${GradlePluginVersion.ANDROID_GRADLE}"
     const val SAFE_ARGS = "androidx.navigation:navigation-safe-args-gradle-plugin:${GradlePluginVersion.SAFE_ARGS}"
+}
