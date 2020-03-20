b69b07c3d579036c9978ab9604aa81f9e86f1854
ulx <alexand.rusin@gmail.com>
2020-03-20 13:28:31 +0300

    fix dashboard

--- feature/dashboard/src/main/java/ru/sberinvestor/dashboard/DashboardFragment.kt
+++ feature/dashboard/src/main/java/ru/sberinvestor/dashboard/DashboardFragment.kt
@@ -7,19 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 
 import ru.sberinvestor.core.fragment.BaseFragment
+import ru.sberinvestor.dashboard.databinding.FmtDashboardSberBinding
 
 
 class DashboardFragment : BaseFragment() {
 
-//    private var _binding: FmtDashboardBinding? = null
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        _binding = FmtDashboardBinding.inflate(inflater, container, false)
-//        return binding.root
-//    }
+    private var _binding: FmtDashboardSberBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FmtDashboardSberBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
 }
