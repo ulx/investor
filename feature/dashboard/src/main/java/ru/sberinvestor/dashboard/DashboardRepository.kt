84c58a2a8b441cff426fe6c2234f8a5e70d27773
ulx <alexand.rusin@gmail.com>
2020-03-26 18:29:40 +0300

    add flow request info

--- feature/dashboard/src/main/java/ru/sberinvestor/dashboard/DashboardRepository.kt
+++ feature/dashboard/src/main/java/ru/sberinvestor/dashboard/DashboardRepository.kt
@@ -1,9 +1,11 @@
 package ru.sberinvestor.dashboard
 
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import ru.sberbank.network.api.InvestorServiceExecutor
 import ru.sberbank.network.entity.Dictionaries
 import ru.sberbank.sberinvestor.extenstion.toAssetDb
@@ -13,6 +15,9 @@ import ru.sberinvestor.core.state.InvestorResult
 
 class DashboardRepository(private val netSource: InvestorServiceExecutor, private val assetDao: AssetDao) {
 
+    /**
+     * Запрос и использованием flow. Может отправлять несколько статусов
+     */
     suspend fun getDictionaries(): Flow<InvestorResult<Dictionaries>> = flow {
         val result = netSource.getDictionaries().toInvestorResult()
         if (result is InvestorResult.Success) {
@@ -30,4 +35,22 @@ class DashboardRepository(private val netSource: InvestorServiceExecutor, privat
         delay(2000)
         emit(result)
     }
+
+    /**
+     * Этот запрос может возвращать только один статус
+     */
+    suspend fun getDictionaries2() : InvestorResult<Dictionaries> {
+        val result = netSource.getDictionaries().toInvestorResult()
+        if (result is InvestorResult.Success){
+            result.data.resp.assets.forEach{
+                Log.i("DashboardRepository", "index = ${assetDao.insert(it.toAssetDb())}")
+            }
+        }
+        withContext(Dispatchers.IO) {
+            val l = assetDao.getAlphabetizedWords()
+            Log.i("DashboardRepository", l.joinToString())
+        }
+        return result
+    }
+
 }
